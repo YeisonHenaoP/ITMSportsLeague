568cd83fcaeb4fcd18d4f449000029b416f635ec
yeison_prestamo <yeisonhenao107513@correo.itm.edu.co>
2026-04-01 22:12:47 -0500

    create SponsorRepository

---
+++ SportsLeague.DataAccess/Repositories/SponsorRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Interfaces.Repositories;
+
+namespace SportsLeague.DataAccess.Repositories;
+
+public class SponsorRepository : GenericRepository<Sponsor>, ISponsorRepository
+{
+    public SponsorRepository(LeagueDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Sponsor?> GetByNameAsync(string name)
+    {
+        return await _dbSet
+            .FirstOrDefaultAsync(s => s.Name.ToLower() == name.ToLower());
+    }
+
+    public async Task<Sponsor?> GetByIdWithTournamentsAsync(int id)
+    {
+        return await _dbSet
+            .Where(s => s.Id == id)
+            .Include(s => s.TournamentSponsors)
+                .ThenInclude(ts => ts.Tournament)
+            .FirstOrDefaultAsync();
+    }
+}
